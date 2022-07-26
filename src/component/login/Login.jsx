f9db5ee31b88d9caba756ecb3f972ea89bad727f
fjcarrozzino <fjcarrozzino@gmail.com>
2022-07-26 00:36:35 -0300

    login

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, TextInput, ScrollView, View, ImageBackground, Dimensions, Pressable, Text } from 'react-native'
+import { StyleSheet, TextInput, ScrollView, View, ImageBackground, Dimensions, Pressable, Text, Alert } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux';
+import userActions from '../../redux/actions/userActions';
 
 
 var { height } = Dimensions.get('window')
@@ -7,12 +9,42 @@ var { height } = Dimensions.get('window')
 export default function Login() {
 
     const [input, setInput] = useState("")
+    const [email, setEmail] = useState({email: ""});
+    const [password, setPassword] = useState({password: ""})
+    const dispatch = useDispatch();
+
+    const message = useSelector(store => store.userReducer.snackbar)
+
+    console.log(message.message)
+
+
+    let carrito=useSelector(store=>store.productReducer.carrito)
+    const loginSubmit  = async (e) => {
+        e.preventDefault();
+        console.log(email)
+        console.log(password)
+        const logedUser={
+          
+          email: email.email,
+          password: password.password,
+          from:'signup'
+        }
+        
+          dispatch(userActions.signInUser(logedUser))
+
+        await Alert.alert(message?.message)
+
+          setEmail("")
+          setPassword("")
+          
+          
+      };
 
     return (
         <View style={styles.backgroundlogin}>
-            <TextInput  placeholder="email" required style={[styles.input, styles.inputtext]} />
-            <TextInput secureTextEntry={true} type="password" placeholder="Contraseña" required style={[styles.input, styles.inputtext]} />
-            <Pressable  style={styles.buttoncontainer} >
+            <TextInput value={email.email}  type="email" placeholder="email" required style={[styles.input, styles.inputtext]} onChangeText={(text) => setEmail({...email, email: text})} />
+            <TextInput secureTextEntry={true} value={password.password} placeholder="Contraseña" required style={[styles.input, styles.inputtext]} onChangeText={(text) => setPassword({...password, password: text})} />
+            <Pressable  style={styles.buttoncontainer} onPress={loginSubmit} >
                 <Text style={[styles.buttonlogin]}>LOGIN</Text>
             </Pressable>
         </View>
